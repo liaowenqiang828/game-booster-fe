3dcf17ffe7248fac50d324c92260b351e115f4ff
liaowenqiang828 <18840965351@163.com>
2023-10-17 22:01:19 +0800

    [wenqiang] feat: add test api config for test env

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8080,
-    proxy: {
-      "/admin/v1": {
-        target: "http://124.221.34.225:10002",
-        changeOrigin: true,
+export default ({ mode }) =>
+  defineConfig({
+    plugins: [react()],
+    server: {
+      port: 8080,
+      proxy: {
+        "/admin/v1": {
+          target: loadEnv(mode, process.cwd()).VITE_APP_API_HOST,
+          changeOrigin: true,
+        },
       },
     },
-  },
-});
+  });
